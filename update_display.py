c14d3640ad7ceec539d66016caf500c034e0571b
Euclid@FrameWork <ouweijie123@outlook.com>
2024-10-28 14:42:38 +0800

    Refactor update_display.py to import additional functions from utils module

--- update_display.py
+++ update_display.py
@@ -1,83 +1,15 @@
-import numpy as np
 import pandas as pd
-from typing import List
-from pyecharts.charts import Line, Grid
-from pyecharts import options as opts
 from pathlib import Path
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from utils import load_hist_data, load_bench_cons, calculate_percentile
-
-
-# Function to generate a line chart
-def plot_line_chart(x_data: np.ndarray, y_data: np.ndarray, name):
-    line = (
-        Line(
-            init_opts={
-                "width": "1560px",
-                "height": "600px",
-                "is_horizontal_center": True,
-            }
-        )
-        .add_xaxis(list(x_data))
-        .add_yaxis(name, list(y_data), is_symbol_show=False)
-        .set_global_opts(
-            xaxis_opts=opts.AxisOpts(type_="category"),
-            yaxis_opts=opts.AxisOpts(type_="value"),
-            legend_opts=opts.LegendOpts(
-                textstyle_opts=opts.TextStyleOpts(font_weight="bold", font_size=20)
-            ),
-            datazoom_opts=[
-                opts.DataZoomOpts(range_start=0, range_end=100, orient="horizontal")
-            ],
-            tooltip_opts=opts.TooltipOpts(trigger="axis"),
-        )
-        .set_series_opts(
-            linestyle_opts=opts.LineStyleOpts(width=3),
-        )
-    )
-    return line
-
-
-# Function to add dual Y-axis chart
-def plot_dual_axis_chart(data, title, key):
-    line1 = (
-        Line()
-        .add_xaxis(list(data["日期"][-100:]))
-        .add_yaxis(
-            f"{key}年化基差(%)",
-            list(data["年化基差(%)"][-100:]),
-            yaxis_index=0,
-            is_smooth=True,
-        )
-        .extend_axis(
-            yaxis=opts.AxisOpts(name="期货&现货价格", type_="value", position="right")
-        )
-    )
-
-    line2 = (
-        Line()
-        .add_xaxis(list(data["日期"][-100:]))
-        .add_yaxis(
-            "现货价格", list(data["现货价格"][-100:]), yaxis_index=1, is_smooth=True
-        )
-        .add_yaxis(
-            "期货价格", list(data["期货价格"][-100:]), yaxis_index=1, is_smooth=True
-        )
-        .set_global_opts(
-            title_opts=opts.TitleOpts(title=title, pos_left="center"),
-            yaxis_opts=opts.AxisOpts(
-                name="年化基差", type_="value", position="left", min_=-20, max_=20
-            ),
-            xaxis_opts=opts.AxisOpts(type_="category"),
-        )
-    )
-
-    grid = Grid()
-    grid.add(line1, grid_opts=opts.GridOpts(pos_left="5%"))
-    grid.add(line2, grid_opts=opts.GridOpts(pos_right="5%"))
-    return grid
-
+from utils import (
+    load_hist_data,
+    load_bench_cons,
+    calculate_percentile,
+    plot_line_chart,
+    plot_dual_y_line_chart,
+    plot_lines_chart,
+)
 
 # Load historical data for 沪深300
 
@@ -93,22 +25,15 @@ if __name__ == "__main__":
         "000852": "中证1000",
     }.items():
         hist_bench_df = pd.read_csv(Path(f"data/hist_of_{bench}.csv"))
-        combined_fig.append(
-            plot_line_chart(
-                hist_bench_df["日期"].values,
-                hist_bench_df["成交金额"].values,
-                f"{name}成交金额",
-            )
-        )
-
         # Calculate and plot 250-day rolling 成交金额 percentile
         VolumeRMB = hist_bench_df["成交金额"].values
         percentile = calculate_percentile(VolumeRMB, 250)
         combined_fig.append(
-            plot_line_chart(
-                hist_bench_df["日期"].values[249:],
-                percentile,
-                f"{name}成交金额分位数",
+            plot_dual_y_line_chart(
+                x_data=hist_bench_df["日期"].values[249:],
+                ys_data=[hist_bench_df["成交金额"].values[249:], percentile],
+                names=[f"{name}成交金额", f"{name}成交金额分位数"],
+                range_start=75,
             )
         )
         # 计算成分股波动率
@@ -117,9 +42,10 @@ if __name__ == "__main__":
         percentile = calculate_percentile(vol[0].values, 250)
         combined_fig.append(
             plot_line_chart(
-                vol.index.strftime("%Y-%m-%d")[249:],
-                percentile,
-                f"{name}成分股波动率分位数",
+                x_data=vol.index.strftime("%Y-%m-%d")[249:],
+                y_data=percentile,
+                name=f"{name}成分股波动率分位数",
+                range_start=75,
             )
         )
         # 赚钱效应
@@ -131,9 +57,10 @@ if __name__ == "__main__":
         win_ratio = ((rtn.values[-250:, :] - bench_rtn.values[-250:]) > 0).mean(axis=1)
         combined_fig.append(
             plot_line_chart(
-                bench_rtn.index.values[-250:],
-                win_ratio,
-                f"{name}赚钱效应",
+                x_data=bench_rtn.index.values[-250:],
+                y_data=win_ratio,
+                name=f"{name}赚钱效应",
+                range_start=75,
             )
         )
 
@@ -159,9 +86,10 @@ if __name__ == "__main__":
     cni_399370["rtn"] = cni_399370["收盘价"].pct_change()
     combined_fig.append(
         plot_line_chart(
-            cni_399371["日期"].values[-100:],
-            cni_399371["rtn"].values[-100:] - cni_399370["rtn"].values[-100:],
-            "价值VS成长相对强弱",
+            x_data=cni_399371["日期"].values[-100:],
+            y_data=cni_399371["rtn"].values[-100:] - cni_399370["rtn"].values[-100:],
+            name="价值VS成长相对强弱",
+            range_start=75,
         )
     )
 
@@ -169,8 +97,14 @@ if __name__ == "__main__":
     IC_data = pd.read_csv(Path(r"data/IC_data.csv"))
     IM_data = pd.read_csv(Path(r"data/IM_data.csv"))
 
-    for key, data in {"IC年化基差(%)": IC_data, "IM年化基差(%)": IM_data}.items():
-        combined_fig.append(plot_line_chart(data["日期"], data["年化基差(%)"], key))
+    combined_fig.append(
+        plot_lines_chart(
+            x_data=IC_data["日期"],
+            ys_data=[IC_data["年化基差(%)"], IM_data["年化基差(%)"]],
+            names=["IC年化基差(%)", "IM年化基差(%)"],
+            range_start=75,
+        )
+    )
 
     html = f"""<html>
         <head>
